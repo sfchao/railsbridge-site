c7970a60aaeed99df2594f18f4864f2546b1a129
MikeRuby <emikebr@gmail.com>
2009-07-04 22:24:41 -0300

    New RailsBridge Network menu bar

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,17 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+// Toggle the RailsBridge Toolbar in an unobtrusive way
+Event.observe(window, 'load', function() { init() });
+function init(){
+	$('menuTrigger').observe('click', function() {
+		triggerEffect(this,'toolbarContainer','slide', 'images/rbnDownButton.png', 'images/rbnUpButton.png');
+	});
+}
+function triggerEffect(srcElement,divId, effect, firstImg, secondImg){
+  Effect.toggle(divId, effect, {
+    beforeStart:function(){
+      image = srcElement.getElementsByTagName('img')[0];
+      image.src = image.src.match(firstImg) ? secondImg : firstImg;
+    },
+	duration:0.2
+  });
+  return false;
+}
