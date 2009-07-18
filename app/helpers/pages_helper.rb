09452dd7e351240f7a364ad742b48cdbc1a7664d
Mike Gunderloy <MikeG1@larkfarm.com>
2009-07-18 08:13:45 -0500

    Rework coding for sponsors page

---
+++ app/helpers/pages_helper.rb
@@ -0,0 +1,9 @@
+module PagesHelper
+  
+  def sponsor(link, image, title, text)
+    "<div class='sponsor'><div class='logo'>" +
+    link_to(image_tag("sponsors/#{image}", {:alt => title, :title => title}), link, :target => "_blank") +
+    "</div><div class='plug'><p>#{text}</p></div><div class='clear'></div></div>"
+   end
+  
+end
