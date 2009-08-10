edaa852a9a80e1daf0ef724f9a5a4741ac389d44
Elad Meidar <elad@eizesus.com>
2009-08-10 13:47:11 -0400

    dynamic url for JSONP.

--- public/javascripts/railsbridge-post-authority-core.js
+++ public/javascripts/railsbridge-post-authority-core.js
@@ -1,18 +1,23 @@
-if (!rails_ver) {
+if (typeof(rails_ver) != "undefined") {
 	var rails_ver = "unknown";
 }
 
-if (!module) {
+if (typeof(module) != "undefined") {
 	var module = "unknown";
 }
 
 var title = $("title").text();
 var post_url = window.location;
 
+if (typeof(debug) == "undefined" ) {
+	var url = "http://railsbridge.org/";
+} else {
+	var url = "http://localhost:3000";
+}
 $.ajax({
   dataType: 'jsonp',
   data: "title=" + title + "&permlink=" + post_url + "&rails_ver=" + rails_ver + "&module=" + module,
-  url: 'http://localhost:3000/post_authorities/get_auth.json',
+  url: url + '/post_authorities/get_auth.json',
   success: function (data) {
 	document.write("This post is: " + data.authority + ". <a href='http://railsbridge.org'>RailsBridge</a>");
   }
