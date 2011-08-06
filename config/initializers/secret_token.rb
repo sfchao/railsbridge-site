793c2cf14397bffc2eec842feffea1389b5c9beb
Nicholas Fowler <nbfowler@gmail.com>
2011-08-06 11:50:15 -0700

    blank app with rspec and cucumber

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railsbridge::Application.config.secret_token = '9430e9a1ec60c71e95a1efce72d5b04d2ab2f7a1ef54a9a953cb7303c92aaee2cf5f08ab5a85a26952cc29044f54fd17ffb4fc490d9b0c32cc3ecd14a9c747b6'
