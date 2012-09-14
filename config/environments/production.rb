cb7bb87c5b837bbbad7a92dc0cac7355744cc76c
shufen Chao <shufen@usamp.com>
2012-09-14 11:56:37 -0500

    Fix broken image issue in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,7 +9,7 @@ Railsbridge::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
@@ -17,6 +17,9 @@ Railsbridge::Application.configure do
   # Specifies the header that your server uses for sending files
   # (comment out if your front-end server doesn't support this)
   config.action_dispatch.x_sendfile_header = "X-Sendfile" # Use 'X-Accel-Redirect' for nginx
+
+  # Generate digests for assets URLs
+  config.assets.digest = true
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
